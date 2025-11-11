ba7252bf85be79433e15dbadea9ddc8cab514353
jcarlossc <josecscosta@fac.pe.senac.br>
2025-11-11 17:00:50 -0300

    ResponseModel

---
+++ src/api_consumer_project/models/ResponseModel.py
@@ -0,0 +1,22 @@
+from dataclass import dataclass, field
+from typing import Any, Optional, Dict
+import datetime
+
+@dataclass
+class ResponseModel:
+    success: bool
+    status_code: int
+    data: Optional[Any] = None
+    message: Optional[str] = None
+    metadata: Dict[str, Any] = field(default_factory = dict)
+    timestamp: str = field(default_factory=lambda: datetime.datetime.utcnow().isoformat())
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "success": self.success,
+            "status_code": self.status_code,
+            "data": self.data,
+            "message": self.message,
+            "metadata": self.metadata,
+            "timestamp": self.timestamp,
+        }
